51208bcff2d41fa06ba2a753eeff4d46dbb9806f
hilton92 <benjamin.hilton92@gmail.com>
2021-05-31 18:41:42 -0700

    initial commit

---
+++ led-cube.ino
@@ -0,0 +1,15 @@
+
+// Author: Ben Hilton
+// Date: May 2021
+
+// scripts for LED cube
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
