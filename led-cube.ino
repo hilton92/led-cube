b275f2994b3cb83d382ccd4d0780362cbfac0c52
hilton92 <benjamin.hilton92@gmail.com>
2021-06-05 21:01:56 -0700

    adding layer functions

--- led-cube.ino
+++ led-cube.ino
@@ -6,6 +6,8 @@
 
 #include <SPI.h>
 
+// initialize global array for cube values (5x5x5)
+bool a[5][5][5]; // [x][y][z] where z is vertical, [0] is base and [4] is top layer
 
 int serialPin = 11; // serial pin out (MOSI pin)
 int clockPin = 13; // clockPin (SCK pin)
@@ -17,23 +19,26 @@ void setup () {
   pinMode(serialPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
-  SPI.begin();
-  Serial.begin(9600);
-  Serial.print("setup complete");
+  
+  SPI.begin(); // begin SPI communication
   //sendFrame(0xC3); // 11011011
 }
 
 
-/*int sendFrame(byte muhByte) { 
+void sendLayer(int layerNum) { 
+  byte byte1 = 
+  
+  
+  
   digitalWrite(latchPin, LOW);
-  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+
   SPI.transfer(muhByte);
   SPI.endTransaction();
   digitalWrite(latchPin, HIGH);
   delay(5);
   digitalWrite(latchPin, LOW);
   Serial.print("function complete");
-}*/
+}
 
 void loop(){
   digitalWrite(latchPin, LOW);
