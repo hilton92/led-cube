da008395bd2be15efcc474a287e84dbefdfda264
hilton92 <benjamin.hilton92@gmail.com>
2021-06-05 20:25:06 -0700

    first working version

--- led-cube.ino
+++ led-cube.ino
@@ -10,23 +10,21 @@
 int serialPin = 11; // serial pin out (MOSI pin)
 int clockPin = 13; // clockPin (SCK pin)
 int latchPin = 6; // Latch pin
-
+byte counter = 0;
 
 void setup () {
   // set output pins as outputs:
-  //pinMode(serialPin, OUTPUT);
-  //pinMode(clockPin, OUTPUT);
+  pinMode(serialPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
   SPI.begin();
-  
- 
   Serial.begin(9600);
   Serial.print("setup complete");
   //sendFrame(0xC3); // 11011011
 }
 
 
-int sendFrame(byte muhByte) { 
+/*int sendFrame(byte muhByte) { 
   digitalWrite(latchPin, LOW);
   SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
   SPI.transfer(muhByte);
@@ -35,14 +33,21 @@ int sendFrame(byte muhByte) {
   delay(5);
   digitalWrite(latchPin, LOW);
   Serial.print("function complete");
-}
+}*/
 
 void loop(){
-  sendFrame(0xDB); // 11011011
+  digitalWrite(latchPin, LOW);
+  SPI.transfer(counter);
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(latchPin, LOW);
+  counter = counter + 1;
+  delay(500);
+  
+  /*sendFrame(0xDB); // 11011011
   delay(1000);
   sendFrame(0xC3); // 11000011
   delay(1000);
   sendFrame(0x81); // 10000001
   delay(1000);
-  Serial.print("loop complete");
+  Serial.print("loop complete");*/
 }
