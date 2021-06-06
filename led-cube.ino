c8ed8dafef419e1a666e61b36f7b69aaee29b9b7
hilton92 <benjamin.hilton92@gmail.com>
2021-06-05 19:04:43 -0700

    first pass, does not work

--- led-cube.ino
+++ led-cube.ino
@@ -4,12 +4,45 @@
 
 // scripts for LED cube
 
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
 
+
+int serialPin = 11; // serial pin out (MOSI pin)
+int clockPin = 13; // clockPin (SCK pin)
+int latchPin = 6; // Latch pin
+
+
+void setup () {
+  // set output pins as outputs:
+  //pinMode(serialPin, OUTPUT);
+  //pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  SPI.begin();
+  
+ 
+  Serial.begin(9600);
+  Serial.print("setup complete");
+  //sendFrame(0xC3); // 11011011
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+int sendFrame(byte muhByte) { 
+  digitalWrite(latchPin, LOW);
+  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+  SPI.transfer(muhByte);
+  SPI.endTransaction();
+  digitalWrite(latchPin, HIGH);
+  delay(5);
+  digitalWrite(latchPin, LOW);
+  Serial.print("function complete");
+}
+
+void loop(){
+  sendFrame(0xDB); // 11011011
+  delay(1000);
+  sendFrame(0xC3); // 11000011
+  delay(1000);
+  sendFrame(0x81); // 10000001
+  delay(1000);
+  Serial.print("loop complete");
 }
